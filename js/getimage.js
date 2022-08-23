3bebf01124051dd321120b5c268d711d9d2ecf54
flatypus <yhc3141@gmail.com>
2022-08-22 18:33:55 -0700

    first commit

---
+++ js/getimage.js
@@ -0,0 +1,17 @@
+function getdir() {
+  var fs = require("fs");
+  var files = fs.readdirSync("./images");
+  console.log(files);
+}
+
+getdir();
+
+// G c s ? N h o h .
+// i o e   e e f e
+// t m m   v a   r
+// H m a   e r
+// u i n   r d
+// b t t
+//     i
+//     c
+//     s
