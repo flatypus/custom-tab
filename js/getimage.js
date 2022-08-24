4689d119ec147bbaef76c0f945d036b795d3f854
flatypus <yhc3141@gmail.com>
2022-08-24 00:57:28 -0700

    |____I

--- js/getimage.js
+++ js/getimage.js
@@ -1,5 +1,4 @@
-function getdir() {
-  console.log(files);
-}
-
-getdir();
+const bg = document.getElementById("background").style;
+bg.backgroundImage = `url("/images/${
+  1 + Math.floor(Math.random() * config.numberofimages)
+}.png")`;
