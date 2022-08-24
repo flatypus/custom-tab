6aa6cd0f30f595e7b1accf05e3ad0b36d7a6d6cf
flatypus <yhc3141@gmail.com>
2022-08-24 02:31:19 -0700

    |G_C_S_?_N_H_H.

--- js/settext.js
+++ js/settext.js
@@ -1,3 +1,19 @@
-console.log(config);
-document.getElementById("title").innerText = config.title;
-document.getElementById("greeting").innerText = `Hello, ${config.name}.`;
+function gettime() {
+  var d = new Date();
+  return d.getTime();
+}
+
+function settext(id, text) {
+  var el = document.getElementById(id);
+  if (el) {
+    el.innerText = text;
+  }
+}
+
+settext("title", config.title);
+settext("greeter", config.name);
+settext("time", gettime())
+
+setInterval(() => {
+  settext("time", gettime());
+}, 1000);
