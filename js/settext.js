f3b72d3874627693928e1b58cc12a604a22fb01c
flatypus <yhc3141@gmail.com>
2022-08-25 16:50:26 -0700

    favicon grabber + localstorage

--- js/settext.js
+++ js/settext.js
@@ -53,11 +53,41 @@ function settext(id, text) {
   }
 }
 
+async function geticon(elem, url) {
+  storage = chrome.storage.local.get("urls", (result) => {
+    if (result.urls[`${url}`] !== undefined) {
+      elem.src = result.urls[`${url}`];
+    } else {
+      fetch(`http://favicongrabber.com/api/grab/` + url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.icons.length > 0) {
+            storage = chrome.storage.local.get("urls");
+            storage[`${url}`] = data.icons[0].src;
+            chrome.storage.local.set({ urls: storage });
+            elem.src = data.icons["0"].src;
+          } else {
+            return "";
+          }
+        });
+      console.log(chrome.storage.local.get("url"));
+    }
+  });
+}
+
 settext("title", config.title);
 settext("greeter", config.name);
 settext("time", gettime());
 settext("date", getdate());
 settext("quote", getQuote());
+img = document.getElementById("lmao");
+geticon(img, "instagram.com");
 
 setInterval(() => {
   settext("time", gettime());
